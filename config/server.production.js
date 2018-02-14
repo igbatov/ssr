680ff8e3a2414b1260796f9229be90e9a31d71f2
Batov <i.batov@megaplan.ru>
2018-02-14 17:44:19 +0300

    project from https://medium.com/front-end-hacking/adding-a-server-side-rendering-support-for-an-existing-react-application-using-express-and-webpack-5a3d60cf9762

---
+++ config/server.production.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    target: 'node',
+    externals:  [nodeExternals()],
+    entry: path.resolve(__dirname, '..', 'src/server/index.js'),
+    output: {
+        path: path.resolve(__dirname, '..', 'dist'),
+        publicPath: '/dist/',
+        filename: 'server.js',
+        library: 'app',
+        libraryTarget: 'commonjs2',
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, '..', 'src/client'),
+        publicPath: '/dist/'
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            components: path.resolve(__dirname, '..', 'src/components'),
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react', 'stage-0']
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ['css-loader/locals']
+            },
+            {
+                test: /\.(ttf|eot|otf|svg|png)$/,
+                loader: 'file-loader?emitFile=false'
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                loader: 'url-loader?emitFile=false'
+            }
+        ]
+    }
+};
